22865c38e982ad89c22fc770e9e9c820da5da71b
zacmea <zach.mears@gmail.com>
2024-04-15 19:19:38 -0400

    event model created

---
+++ backend/models/events.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+    name: String,
+    date: Date,
+    startLocalTime: int32,
+    endLocalTime: int32,
+    description: String,
+    address: String,
+    city: String,
+    state: String,
+    zip: String,
+    attractionNames: [String],
+    imageURL: String,
+    tmID: String,
+    genreClassifications: [String],
+    lalUsersAttending: int32
+});
+
+const Event = mongoose.model('Event', eventSchema);
+module.exports = Event;
