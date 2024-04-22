f574e79ecb80ef82dba63b3b7a427c8fa3b2c489
zacmea <zach.mears@gmail.com>
2024-04-22 15:50:41 -0400

    trying to merge down from dev

---
+++ frontend/src/pages/EventShowPage.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const EventShowPage = () => {
+    //js code
+    const params = useParams();
+    const {
+        selectedEvent,
+        name,
+        date,
+        startLocalTime,
+        imageURL,
+        attractionNames,
+        locationName,
+        address,
+        city,
+        state,
+        zip,
+        description,
+        userUpcomingEvents,
+        comingSoonNearUser,
+        userMightLike,
+        lalUsersAttending,
+    } = props;
+
+    //HTML return
+    return (
+        <>
+            <nav />
+            <h1>{name}</h1>
+            <img src={imageURL} alt={name} />
+            <hr />
+            <h2>`${attractionNames} at ${locationName}`</h2>
+            <h3>`Buy Tickets: ${TICKET-LINK-HERE}`</h3>
+            <p>`${address}, ${city}, ${state} ${zip}`</p>
+            <p>`${date} at ${startLocalTime} - ${endLocalTime}`</p>
+            <p>{description}</p>
+            <br />
+            <br />
+            <h4>`${lalUsersAttending} Locate-a-Laugh users attending`</h4>
+        </>
+    );
+};
+
+export default EventShowPage;
