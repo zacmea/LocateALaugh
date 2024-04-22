fe8357b5c5974d48109ef6eba84caf74b1c3638e
zacmea <zach.mears@gmail.com>
2024-04-22 15:56:33 -0400

    conflicts resolved

--- frontend/src/pages/EventShowPage.jsx
+++ frontend/src/pages/EventShowPage.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import { useParams } from "react-router-dom";
 
 const EventShowPage = () => {
-<<<<<<< HEAD
-    //js code
-=======
     //code
->>>>>>> df99cdf5dcbb1dbcbb43e968a8d0f968ee392b7b
     const params = useParams();
     const {
         selectedEvent,
@@ -35,10 +31,7 @@ const EventShowPage = () => {
             <img src={imageURL} alt={name} />
             <hr />
             <h2>`${attractionNames} at ${locationName}`</h2>
-<<<<<<< HEAD
             <h3>`Buy Tickets: ${TICKET-LINK-HERE}`</h3>
-=======
->>>>>>> df99cdf5dcbb1dbcbb43e968a8d0f968ee392b7b
             <p>`${address}, ${city}, ${state} ${zip}`</p>
             <p>`${date} at ${startLocalTime} - ${endLocalTime}`</p>
             <p>{description}</p>
